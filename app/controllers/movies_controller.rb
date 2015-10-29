486ceae4473d651a844f72223da111f09d4bace7
vagrant <vagrant@lighthouse-labs>
2015-10-29 17:50:10 +0000

    added the search feature for movies, either by: title, director or runtime.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.search(params[:title], params[:director], params[:runtime])
   end
 
   def show
