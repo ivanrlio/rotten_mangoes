3ab3618057b4f58255cad5805239ae4a5356d327
vagrant <vagrant@lighthouse-labs>
2015-10-29 19:57:42 +0000

    Added welcome and  delete notifications upon user sign up and user deletion.  Also added an admin user(new/edit) form that allows the admin to create/edit users, in addition to setting admin status existing/new users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,19 @@ class Admin::UsersController < UsersController
   before_filter :user_is_admin
 
   def index
-    @users = User.all.page(params[:page]).per(5)
+    @users = User.all.page(params[:page]).per(5)  
+  end
+
+  def create
+    binding.pry
+    @user = User.new(user_params)
+  
+    if @user.save
+      UserMailer.welcome_email(@user).deliver
+      redirect_to(admin_users_path, notice: "User was successfully created")
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,7 +30,7 @@ class Admin::UsersController < UsersController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to admin_user(@user)
+      redirect_to admin_user_path(@user)
     else
       render :edit
     end
@@ -27,8 +39,19 @@ class Admin::UsersController < UsersController
   def destroy
     @user = User.find(params[:id])
     user = @user
-    @user.destroy
-    redirect_to admin_users_path, notice: "#{user.full_name} was successfully deleted."
-  end 
+  
+    if @user.destroy
+      UserMailer.delete_email(user).deliver
+       redirect_to(admin_users_path, notice: "#{user.full_name} was successfully deleted.")
+    else
+      redirect admin_users_path, alert: "User was NOT deleted or could not be found!"
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 
 end
